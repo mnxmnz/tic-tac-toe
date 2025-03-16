0417293582fa1caf653794ee6d90ab210017a68c
mnxmnz <minzidev@gmail.com>
2025-03-16 21:18:59 +0900

    feat: rename onClickSquare to handleClick for clarity

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -18,7 +18,7 @@ function Board({ currentPlayer, squares, onPlay }: BoardProps) {
     onPlay(newSquares);
   };
 
-  const onClickSquare = (index: number) => {
+  const handleClick = (index: number) => {
     if (squares[index] || winner) {
       return;
     }
@@ -32,19 +32,19 @@ function Board({ currentPlayer, squares, onPlay }: BoardProps) {
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onClick={() => onClickSquare(0)} />
-        <Square value={squares[1]} onClick={() => onClickSquare(1)} />
-        <Square value={squares[2]} onClick={() => onClickSquare(2)} />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onClick={() => onClickSquare(3)} />
-        <Square value={squares[4]} onClick={() => onClickSquare(4)} />
-        <Square value={squares[5]} onClick={() => onClickSquare(5)} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onClick={() => onClickSquare(6)} />
-        <Square value={squares[7]} onClick={() => onClickSquare(7)} />
-        <Square value={squares[8]} onClick={() => onClickSquare(8)} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </>
   );
