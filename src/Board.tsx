f620ca85a71819ddcdea6b019258f3aa7e1d8fec
mnxmnz <minzidev@gmail.com>
2025-03-16 20:41:46 +0900

    feat: update Board component to manage player turns

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,32 +1,53 @@
 import { useState } from 'react';
 
-import Cell from './Cell';
+import Square from './Square';
+
+type Player = 'X' | 'O';
 
 function Board() {
-  const [cells, setCells] = useState<string[]>(Array(9).fill(null));
+  const [player, setPlayer] = useState<Player>('X');
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
 
-  const handleClick = (index: number) => {
-    const newCells = [...cells];
-    newCells[index] = 'X';
-    setCells(newCells);
+  const switchTurn = () => {
+    switch (player) {
+      case 'X':
+        setPlayer('O');
+        break;
+      case 'O':
+        setPlayer('X');
+        break;
+      default:
+        setPlayer('X');
+    }
+  };
+
+  const makeMove = (index: number) => {
+    const newSquares = [...squares];
+    newSquares[index] = player;
+    setSquares(newSquares);
+  };
+
+  const onClickSquare = (index: number) => {
+    makeMove(index);
+    switchTurn();
   };
 
   return (
     <>
       <div className="board-row">
-        <Cell value={cells[0]} onClick={() => handleClick(0)} />
-        <Cell value={cells[1]} onClick={() => handleClick(1)} />
-        <Cell value={cells[2]} onClick={() => handleClick(2)} />
+        <Square value={squares[0]} onClick={() => onClickSquare(0)} />
+        <Square value={squares[1]} onClick={() => onClickSquare(1)} />
+        <Square value={squares[2]} onClick={() => onClickSquare(2)} />
       </div>
       <div className="board-row">
-        <Cell value={cells[3]} onClick={() => handleClick(3)} />
-        <Cell value={cells[4]} onClick={() => handleClick(4)} />
-        <Cell value={cells[5]} onClick={() => handleClick(5)} />
+        <Square value={squares[3]} onClick={() => onClickSquare(3)} />
+        <Square value={squares[4]} onClick={() => onClickSquare(4)} />
+        <Square value={squares[5]} onClick={() => onClickSquare(5)} />
       </div>
       <div className="board-row">
-        <Cell value={cells[6]} onClick={() => handleClick(6)} />
-        <Cell value={cells[7]} onClick={() => handleClick(7)} />
-        <Cell value={cells[8]} onClick={() => handleClick(8)} />
+        <Square value={squares[6]} onClick={() => onClickSquare(6)} />
+        <Square value={squares[7]} onClick={() => onClickSquare(7)} />
+        <Square value={squares[8]} onClick={() => onClickSquare(8)} />
       </div>
     </>
   );
